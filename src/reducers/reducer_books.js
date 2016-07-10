7cedba3d8c5bbe20278720e0bfe7a47d8c8fed27
burak <tknbrk@gmail.com>
2016-07-10 22:48:11 +0300

    remove wrong placed folder.

--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    {title: 'JS: The good parts'},
-    {title: "Harry Potter and the Sorcerer's Stone"},
-    {title: 'The Matrix'},
-    {title: 'Life of Alon Musk'}
-  ];
-}
